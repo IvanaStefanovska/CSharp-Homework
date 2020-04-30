cafea61c3499be9fe03605fc97f9cc3b1d938a72
IvanaStefanovska <ivstefanovska21@gmail.com>
2020-04-30 15:23:35 +0200

    Added Homework Inheritance

---
+++ Songs/DomainModels/Genre.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Services;
+
+namespace DomainModels
+{
+
+        public enum Genre
+        {
+            Rock,
+            Jazz,
+            Disco,
+            Techno,
+            Instrumental
+        }
+
+}
---
+++ Songs/DomainModels/SongModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Services;
+
+namespace DomainModels
+{
+    public class SongModel
+    {
+
+        public SongModel()
+        {
+            Name = "Bohemian Rapsody";
+            Band = "Queen";
+            Genre = Genre.Rock;
+            Language = "english";
+            Length = 180;
+            Author = "Bryan";
+        }
+        public SongModel(string name, string band, Genre genre, string language, int length, string author)
+        {
+            Name = name;
+            Author = author;
+            Genre = genre;
+            Language = language;
+            Band = band;
+            Length = length;
+        }
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public Genre Genre { get; set; }
+        public string Language { get; set; }
+        public int Length { get; set; }
+        public string Band { get; set; }
+
+    }
+}
